e00d184dccdc99e0853637d6c3791f5dd3de0020
davidvalbuenae <davidfa9718@gmail.com>
2022-12-20 13:14:31 -0500

    Primer commit de inicialización

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frontend admin AVE
+
+Este es el frontend de admin ave, iniciado en react junto con vite
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="hora">hpña</h1>
+    </div>
+  )
+}
+
+export default App
